67adb5c4ea59719bf250be8234ab4717c478a6c8
lamthonn <lamvu180903@gmail.com>
2024-03-16 09:48:18 +0700

    api

--- src/constants/api.js
+++ src/constants/api.js
@@ -4,9 +4,14 @@ const localPath = app_config.LOCAL_PATH
 const apiUrl = {
     //Authentication
     DANG_NHAP: `${localPath}/api/Auth/Login`,
+    DANG_KY_FACEBOOK: `${localPath}/api/Auth/RegisterWithFacebook`,
     DANG_KY: `${localPath}/api/Auth/Register`,
+    CHECK_TOKEN_FACEBOOK:`${localPath}/api/Auth/ValidateFacebookToken`,
 
     //User
     GET_USER_BY_ID:`${localPath}/api/User/GetUserbyId`,
+
+    //học phần
+    THEM_HOC_PHAN:`${localPath}/api/HocPhan/ThemHocPhan`
 }
 export default apiUrl;
